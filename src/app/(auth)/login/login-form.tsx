54e7dc0d6fe30639dd2ac61490b49c5593412d7d
NguyenDuy <admin@VSMI-006.vsmi.network>
2024-10-08 14:46:01 +0700

    update form

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,11 +1,47 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const LoginSchema = z.object({
+    id: z.string({
+      message: "Vui lòng nhập id",
+    }),
+    password: z
+      .string({
+        message: "Vui lòng nhập mật khẩu",
+      })
+      .min(6, {
+        message: "Vui lòng nhập ít nhất 6 ký tự",
+      }),
+  });
+
+  type LoginSchemaType = z.infer<typeof LoginSchema>;
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    getValues,
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(LoginSchema),
+    mode: "onSubmit",
+  });
+
+  const onSubmit: SubmitHandler<LoginSchemaType> = async (data) => {
+    if (data) {
+      console.log("data", data);
+    }
+  };
   return (
     <div>
       <h2>Sign In</h2>
-      
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        
+      </form>
     </div>
   );
 };
